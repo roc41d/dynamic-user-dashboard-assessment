8162a37f54ae0d7771ee3a1f68698e685683209b
Rocard Fonji <fonkengrocard@gmail.com>
2024-06-24 11:49:25 +0100

    (feature: list-users): get and display user data from NgRx store

--- src/app/users/feature/list-users/list-users.component.ts
+++ src/app/users/feature/list-users/list-users.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
+import {
+  selectLoading,
+  getUsers,
+  selectError,
+  selectTotal,
+} from './data-access/store/reducers';
+import { listUsersActions } from './data-access/store/actions';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-list-users',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './list-users.component.html',
-  styleUrl: './list-users.component.scss'
+  styleUrl: './list-users.component.scss',
 })
-export class ListUsersComponent {
+export class ListUsersComponent implements OnInit {
+  private currentPage = 1;
+  private store = inject(Store);
 
+  data$ = combineLatest({
+    isLoading: this.store.select(selectLoading),
+    users: this.store.select(getUsers),
+    error: this.store.select(selectError),
+    total: this.store.select(selectTotal),
+  });
+
+  ngOnInit(): void {
+    this.loadUsers(this.currentPage);
+  }
+
+  loadUsers(page: number): void {
+    this.store.dispatch(listUsersActions.getUsers({ page }));
+  }
 }
--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,4 +1,11 @@
 import { Route } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideState } from '@ngrx/store';
+import * as listUsersEffect from './feature/list-users/data-access/store/effects';
+import {
+  listUsersFeatureKey,
+  listUsersReducer,
+} from './feature/list-users/data-access/store/reducers';
 
 export const USERS_ROUTES: Route[] = [
   {
@@ -7,5 +14,9 @@ export const USERS_ROUTES: Route[] = [
       import('./feature/list-users/list-users.component').then(
         (comp) => comp.ListUsersComponent,
       ),
+    providers: [
+      provideState(listUsersFeatureKey, listUsersReducer),
+      provideEffects(listUsersEffect),
+    ],
   },
 ];
