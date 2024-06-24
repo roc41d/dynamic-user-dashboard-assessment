f47ecf3d88e0f2c9832b88bf91c1386bc3674abc
Rocard Fonji <fonkengrocard@gmail.com>
2024-06-24 15:15:13 +0100

    test: cleanup app component and add test cases for user details compoonent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideMockStore({ })]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'dynamic-user-dashboard-assessment' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('dynamic-user-dashboard-assessment');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dynamic-user-dashboard-assessment');
+    expect(component).toBeTruthy();
   });
 });
--- src/app/users/feature/user-details/user-details.component.spec.ts
+++ src/app/users/feature/user-details/user-details.component.spec.ts
@@ -1,23 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { provideMockStore } from '@ngrx/store/testing';
 import { UserDetailsComponent } from './user-details.component';
+import { userDetailActions } from './data-access/store/actions';
 
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
+  let route: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserDetailsComponent]
-    })
-    .compileComponents();
+      imports: [UserDetailsComponent],
+      providers: [
+        provideMockStore({ }),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '1',
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserDetailsComponent);
     component = fixture.componentInstance;
+    route = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch getUserDetails action on ngOnInit', () => {
+    const userId = parseInt(route.snapshot.paramMap.get('id')!, 10);
+    jest.spyOn(component['store'], 'dispatch').getMockImplementation();
+    component.ngOnInit();
+    expect(component['store'].dispatch).toHaveBeenCalledWith(
+      userDetailActions.getUserDetails({ userId })
+    );
   });
 });
