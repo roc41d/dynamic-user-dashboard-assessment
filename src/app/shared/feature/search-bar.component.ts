f20f45eda93224ad009fd84263359e66b75ea930
Rocard Fonji <fonkengrocard@gmail.com>
2024-06-24 14:08:32 +0100

    (feature: search-bar): implement search

--- src/app/shared/feature/search-bar.component.ts
+++ src/app/shared/feature/search-bar.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs';
+import { listUsersActions } from '../../users/feature/list-users/data-access/store/actions';
 
 @Component({
   selector: 'app-search-bar',
@@ -10,7 +13,7 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
       matInput
       class="search-input"
       [formControl]="searchInput"
-      placeholder="Search"
+      placeholder="Search by ID"
       type="text"
     />
   `,
@@ -31,6 +34,26 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
     `,
   ],
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
   public searchInput = new FormControl('');
+
+  private store = inject(Store);
+
+  ngOnInit(): void {
+    this.searchInput.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        tap((searchText) => {
+          if (!searchText) {
+            this.store.dispatch(listUsersActions.getUsers({ page: 1 }));
+          } else {
+            this.store.dispatch(
+              listUsersActions.updateUserSearchText({ searchText }),
+            );
+          }
+        }),
+      )
+      .subscribe();
+  }
 }
