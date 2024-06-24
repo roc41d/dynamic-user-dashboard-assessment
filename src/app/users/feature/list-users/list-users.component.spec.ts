47f87f2e25e9d86f76251ec71ab56788b93d5d31
Rocard Fonji <fonkengrocard@gmail.com>
2024-06-24 16:00:32 +0100

    (test: users): test cases for list users and user card

--- src/app/users/feature/list-users/list-users.component.spec.ts
+++ src/app/users/feature/list-users/list-users.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListUsersComponent } from './list-users.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { listUsersActions } from './data-access/store/actions';
+import { PageEvent } from '@angular/material/paginator';
 
 describe('ListUsersComponent', () => {
   let component: ListUsersComponent;
@@ -8,16 +10,33 @@ describe('ListUsersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListUsersComponent]
-    })
-    .compileComponents();
+      imports: [ListUsersComponent],
+      providers: [provideMockStore({})],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ListUsersComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load users on initialization', () => {
+    jest.spyOn(component['store'], 'dispatch').getMockImplementation();
+    component.ngOnInit();
+    expect(component['store'].dispatch).toHaveBeenCalledWith(
+      listUsersActions.getUsers({ page: 1 }),
+    );
+  });
+
+  it('should load users when page changes', () => {
+    jest.spyOn(component['store'], 'dispatch').getMockImplementation();
+    const pageEvent = { pageIndex: 2 } as PageEvent;
+    component.pageChange(pageEvent);
+    expect(component['currentPage']).toBe(3);
+    expect(component['store'].dispatch).toHaveBeenCalledWith(
+      listUsersActions.getUsers({ page: 3 }),
+    );
   });
 });
--- src/app/users/feature/list-users/ui/user-card/user-card.component.spec.ts
+++ src/app/users/feature/list-users/ui/user-card/user-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserCardComponent } from './user-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { User } from '../../../../interfaces/user';
+import { ActivatedRoute } from '@angular/router';
 
 describe('UserCardComponent', () => {
   let component: UserCardComponent;
@@ -8,16 +10,53 @@ describe('UserCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserCardComponent]
-    })
-    .compileComponents();
+      imports: [UserCardComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => '1'
+              }
+            }
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a user input', () => {
+    const mockUser: User = {
+      id: 1,
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john.doe@example.com',
+      avatar: 'https://example.com/avatar.jpg',
+    };
+    component.user = mockUser;
+    fixture.detectChanges();
+
+    const cardElement = fixture.nativeElement.querySelector('mat-card');
+    expect(cardElement).toBeTruthy();
+
+    const nameElement = cardElement.querySelector('.mat-mdc-card-title');
+    expect(nameElement.textContent).toContain(
+      `${mockUser.first_name} ${mockUser.last_name}`,
+    );
+
+    const emailElement = cardElement.querySelector('.mat-mdc-card-subtitle');
+    expect(emailElement.textContent).toContain(mockUser.email);
+  });
+
+  it('should not render if user input is not provided', () => {
+    expect(() => fixture.detectChanges()).toThrowError("Cannot read properties of undefined (reading 'id')");
   });
 });
